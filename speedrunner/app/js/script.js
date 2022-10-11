6fa2c5519f91b4cee059205dca59b7013deb52a0
Dan Alex <daniil.alex1@gmail.com>
2022-10-11 22:21:41 +0300

    add slider script

--- speedrunner/app/js/script.js
+++ speedrunner/app/js/script.js
@@ -1,40 +1,13 @@
-$(document).ready(function () {
-  $(".carousel__inner").slick({
-    dots: false,
-    infinite: true,
-    speed: 1200,
-    slidesToShow: 1,
-    adaptiveHeight: false,
-    autoplay: false,
-    autoplaySpeed: 2000,
-    fade: true,
-    cssEase: "linear",
+const slider = tns({
+  container: ".carousel__inner",
+  items: 1,
+  slideBy: "page",
+  autoplay: false,
+});
 
-    prevArrow:
-      '<button type="button" class="slick-prev"><img src = ../img/slider/chevron-left-solid.png /></button>',
-    nextArrow:
-      '<button type="button" class="slick-next"><img src = ../img/slider/chevron-right-solid.png /></button>',
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: "40px",
-          slidesToShow: 1,
-          dots: true,
-          fade: true,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: "40px",
-          slidesToShow: 1,
-        },
-      },
-    ],
-  });
+document.querySelector(".prev").addEventListener("click", () => {
+  slider.goTo("prev");
+});
+document.querySelector(".next").addEventListener("click", () => {
+  slider.goTo("next");
 });
