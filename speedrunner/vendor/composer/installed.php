69c57ccb863150194220806a2d54f7b4be132ebc
Dan Alex <daniil.alex1@gmail.com>
2022-11-03 23:03:27 +0200

    ad composer with vendor

---
+++ speedrunner/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'dan/speed',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6dc520fc4c41ec820d405d25f91a3f38b1713b07',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dan/speed' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6dc520fc4c41ec820d405d25f91a3f38b1713b07',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.6.5',
+            'version' => '6.6.5.0',
+            'reference' => '8b6386d7417526d1ea4da9edb70b8352f7543627',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
