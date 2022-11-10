7d6e6cd52aaf7228460931a6effe56537ff7f558
Dan Alex <daniil.alex1@gmail.com>
2022-11-10 23:50:29 +0200

    fixed: script for page up only

--- speedrunner/app/js/pageup.js
+++ speedrunner/app/js/pageup.js
@@ -1,15 +1,14 @@
 $(document).ready(function () {
-$(window).scroll(function() {
+  $(window).scroll(function () {
     if ($(this).scrollTop() > 1600) {
-        $('.pageup').fadeIn();
+      $(".pageup").fadeIn();
     } else {
-        $('.pageup').fadeOut();
+      $(".pageup").fadeOut();
     }
-});
-$("a[href^='#']").click(function(){
+  });
+  $("a[href=#up]").click(function () {
     const _href = $(this).attr("href");
-    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
     return false;
-});
-
+  });
 });
